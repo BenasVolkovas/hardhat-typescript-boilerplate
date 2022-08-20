69e1e4e7ccd02592669f373b9f48f864c50e7dc5
Benas Volkovas <49361502+BenasVolkovas@users.noreply.github.com>
2022-08-20 14:57:49 +0300

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,5 @@
 import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-solhint";
 import "dotenv/config";
 
 const GOERLI_RPC_URL =
